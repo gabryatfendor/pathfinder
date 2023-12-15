8984678c704d4ef69f8bb4141b391ffc4665acde
gabryatfendor <sani.gabriele@protonmail.com>
2023-12-15 09:05:34 +0000

    feat: added real-time render

--- js/map.js
+++ js/map.js
@@ -5,19 +5,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-var geojsonFeature = {
-	"type": "Feature",
-	"properties": {
-		"name": "San Domenico in Campigno",
-		"amenity": "Church",
-		"popupContent": "Punto di partenza"
-	},
-	"geometry": {
-		"type": "Point",
-		"coordinates": [11.5914, 44.0199]
-	}
-};
-
 var markerOptions = {
 	radius: 8,
 	fillColor: "#f54254",
@@ -33,9 +20,66 @@ function onEachFeature(feature, layer) {
 	}
 }
 
-L.geoJSON(geojsonFeature, {
-	onEachFeature: onEachFeature,
-	pointToLayer: function (feature, latlng) {
-		return L.circleMarker(latlng, markerOptions);
+async function updatePoints() {
+	var NE = map.getBounds()._northEast;
+	var SW = map.getBounds()._southWest;
+	//SWNE is the order for overpass bbox definition 
+	//nwr(S,W,N,E)["fixme"="continue"]
+	
+	var overpassData = await getOverpassData(SW.lat, SW.lng, NE.lat, NE.lng);
+	var geojson = jsonToGeoJson(overpassData);
+
+	L.geoJSON(geojson, {
+		onEachFeature: onEachFeature,
+		pointToLayer: function (feature, latlng) {
+			return L.circleMarker(latlng, markerOptions);
+		}
+	}).addTo(map);
+}
+
+function getOverpassData(south, west, north, east) {
+	var apiCall = "https://overpass-api.de/api/interpreter?data=%5Bout%3Ajson%5D%5Btimeout%3A25%5D%3B%0Anwr%28" + south + "%2C" + west + "%2C" + north + "%2C" + east + "%29%5B%22fixme%22%3D%22continue%22%5D%3B%0Aout%20geom%3B";
+	return fetch(apiCall)
+		.then(response => {
+			if (!response.ok) {
+				throw new Error('nada');
+			}
+			return response.json();
+		})
+		.then(data => {
+			return data;
+		})
+		.catch(error => {
+			console.error('Error:', error);
+		});
+}
+
+function jsonToGeoJson(json) {
+	var geojson = {   type: "FeatureCollection",   features: [], };
+	
+	for (i = 0; i < json.elements.length; i++) {
+		geojson.features.push({
+			"type": "Feature",
+			"geometry": {
+				"type": "Point",
+				"coordinates": [json.elements[i].lon, json.elements[i].lat]
+			},
+			"properties": {
+				"popupContent": "fixme:continue"
+			}
+		});
 	}
-}).addTo(map);
+
+	return geojson;
+}
+
+//first map update
+updatePoints();
+
+map.on('zoomend', function() {
+	updatePoints();
+});
+
+map.on('dragend', function() {
+	updatePoints();
+});
